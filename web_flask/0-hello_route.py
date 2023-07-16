5c7b6f23beb90894f6f98700025add7c9ed87ad9
EngrOgonna <goddeyalaba11@gmail.com>
2023-07-16 04:46:18 +0000

    cool

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/')
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
